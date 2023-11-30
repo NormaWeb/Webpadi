9e69f78ef1ca2f211d23632fbfabd9a8f3d9c80e
Chris Asek <chrisasek@gmail.com>
2023-11-30 01:27:19 +0100

    Client Section Fix

--- src/sections/Onlinebusiness.jsx
+++ src/sections/Onlinebusiness.jsx
@@ -13,105 +13,82 @@ import Image from '../assets/Image1.png';
 const Onlinebusiness = () => {
   return (
     <section>
-      <div>
-        <h1 className='text-white text-4xl font-semibold'>We have helped 50+ businesses <br /> get online and earn more. </h1>
+      <div className='lg:max-w-[65%] mb-10'>
+        <h1 className='text-white text-3xl/snug lg:text-4xl font-semibold'>We have helped 50+ businesses get online and earn more.</h1>
       </div>
 
-      <div className="flex justify-around  max-lg:flex-col  mt-8">
+      <div className="flex justify-around  max-lg:flex-col">
 
-        <div className="flex-1 flex-col md:w-1/3 lg:w-1/2">
+        <div className="columns-2 lg:columns-3 gap-8 grow">
 
-          <div className="mt-8">
-            <img src={Icon1} alt=""
-              width={130}
+          <div className="flex lg:items-center h-[72px] lg:h-[120px]  w-[104px] lg:w-[100%]">
+            <img src={Icon1} alt="" width={150}
               className="object-contain"
             />
           </div>
 
-          <div className="mt-16">
-            <img src={Icon2} alt=""
-              width={130}
+          <div className="flex lg:items-center h-[72px] lg:h-[120px]  w-[104px] lg:w-[100%]">
+            <img src={Icon2} alt="" width={150}
               className="object-contain"
             />
           </div>
 
-          <div className="mt-16">
-            <img src={Icon3} alt=""
-              width={130}
-              className="object-contain"
-            />
-          </div>
-        </div>
-
-
-        <div className="flex-1 flex-col md:w-1/3 lg:w-1/2">
-
-          <div className="mt-10">
-            <img src={Icon4} alt=""
-              width={130}
-              className="object-contain"
-            />
-          </div>
-          <div className="mt-16">
-            <img src={Icon5} alt=""
-              width={130}
-              className="object-contain"
-            />
-          </div>
-          <div className="mt-16">
-            <img src={Icon6} alt=""
-              width={130}
-              className="object-contain"
-            />
-          </div>
-        </div>
-
-
-        <div className="flex-1 flex-col md:w-1/3 lg:w-1/2 ">
-
-          <div className="mt-8">
-            <img src={Icon7} alt=""
-              width={130}
+          <div className="flex lg:items-center h-[72px] lg:h-[120px]  w-[104px] lg:w-[100%]">
+            <img src={Icon3} alt="" width={150}
               className="object-contain"
             />
           </div>
 
-          <div className="mt-16">
-            <img src={Icon8} alt=""
-              width={130}
+          <div className="flex lg:items-center h-[72px] lg:h-[120px]  w-[104px] lg:w-[100%]">
+            <img src={Icon4} alt="" width={150}
+              className="object-contain"
+            />
+          </div>
+          <div className="flex lg:items-center h-[72px] lg:h-[120px]  w-[104px] lg:w-[100%]">
+            <img src={Icon5} alt="" width={150}
+              className="object-contain"
+            />
+          </div>
+          <div className="flex lg:items-center h-[72px] lg:h-[120px]  w-[104px] lg:w-[100%]">
+            <img src={Icon6} alt="" width={150}
               className="object-contain"
             />
           </div>
 
-          <div className="mt-16">
-            <img src={Icon2} alt=""
-              width={130}
+          <div className="flex lg:items-center h-[72px] lg:h-[120px]  w-[104px] lg:w-[100%]">
+            <img src={Icon7} alt="" width={150}
+              className="object-contain"
+            />
+          </div>
+
+          <div className="flex lg:items-center h-[72px] lg:h-[120px]  w-[104px] lg:w-[100%]">
+            <img src={Icon8} alt="" width={150}
+              className="object-contain"
+            />
+          </div>
+
+          <div className="flex lg:items-center h-[72px] lg:h-[120px]  w-[104px] lg:w-[100%]">
+            <img src={Icon2} alt="" width={150}
               className="object-contain"
             />
           </div>
         </div>
 
-
-        <div className="flex-1 flex-col ml-10 md:w-2/3 lg:w-1/2">
-          <div className="mt-5 relative">
-            <blockquote className='text-white text-xl relative pl-8'>
-              <span className="absolute left-0 top-0 text-4xl">&ldquo;</span>
-              I can tell you first hand, Basecom is <br /> absolutely amazing. They have helped me <br /> grow my company by 125% since they <br /> revamped our website and messaging. <br /> Highly Recommended!
-            </blockquote>
+        <div className="flex-1 flex-col lg:ml-10 md:w-2/3 lg:w-1/2">
+          <div className="mt-5">
+            <span className="text-7xl/none text-white">&ldquo;</span>
+            <p className='text-white text-xl/snug'>
+              I can tell you first hand, Basecom is absolutely amazing. They have helped me grow my company by 125% since they revamped our website and messaging. Highly Recommended!
+            </p>
           </div>
 
-          <div className='flex '>
+          <div className='flex gap-3 mt-8'>
+            <img src={Image} alt=""
+              width={100}
+              className="object-cover rounded-full w-[50px] h-[50px]" />
+
             <div className='flex-1 flex-col'>
-              <img src={Image} alt=""
-                width={100}
-                className="object-contain rounded-2xl mt-8"
-              // className="w-12 h-12 rounded-full object-cover"
-              />
-            </div>
-            <div className='flex-1 flex-col mt-8'>
-              <span className=" text-xl text-right   text-white">
-                David Henricks
-              </span>
+              <span className="text-white text-lg font-semibold">David Henricks</span>
               <p className='text-white text-sm mt-1'>CEO, Henricks Enterprise</p>
             </div>
           </div>
--- src/sections/Smallbusiness.jsx
+++ src/sections/Smallbusiness.jsx
@@ -7,19 +7,14 @@ const Smallbusiness = () => {
         <div className="">
             <div className='lg:max-w-[65%]'>
                 <h1 className='text-5xl lg:text-7xl/none font-semibold mb-5'>Take your small business online today.</h1>
-                <p className='text-[23px]/normal md:text-3xl/snug mb-6'>Get a high-converting website for your business so 
-                    your customers and potential customers see and know 
-                    what you do/sell with a click of a button.</p> 
+                <p className='text-[23px]/normal md:text-3xl/snug mb-6'>Get a high-converting website for your business so
+                    your customers and potential customers see and know
+                    what you do/sell with a click of a button.</p>
 
                 <Button label="Get Started" />
             </div>
 
-
-            <div className='mt-10'>
-                < img src={Image1} alt="Create an Account"
-                    className="object-contain"
-                />
-            </div>
+            <img src={Image1} alt="Create an Account" className="object-contain mt-10" />
         </div>
     )
 }
