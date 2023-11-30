6c42d9b497529a11bdbb674a56833d6a064d0272
Chris Asek <chrisasek@gmail.com>
2023-11-30 05:20:42 +0100

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 function Footer() {
   return (
     <footer className="bg-primary padding">
-      <div className="container mx-auto flex flex-col lg:flex-row justify-between items-center">
-        <div className="text-white text-2xl font-semibold mb-4 lg:mb-0 lg:mr-6"> Logo</div>
+      <div className="flex flex-col lg:flex-row lg:justify-between lg:items-center">
+        <div className="text-white text-2xl font-semibold mb-14 lg:mb-0"> Logo</div>
 
-        <ul className="flex space-x-10 gap-10 text-white mb-4 lg:mb-0 ">
+        <ul className="flex flex-col lg:flex-row lg:space-x-10 gap-10 text-white mb-4 lg:mb-0 ">
           <li><a href="#" className="hover:text-gray-300 text-xl">Home</a></li>
           <li><a href="#" className="hover:text-gray-300 text-xl">Terms of Use</a></li>
           <li><a href="#" className="hover:text-gray-300 text-xl">Privacy and Cookies Policy</a></li>
@@ -14,7 +14,7 @@ function Footer() {
       </div>
 
       <div className="text-white text-xl mt-10">
-        All right reserved  &copy; 2023 | Oniontabs LLC
+        All right reserved  &copy; 2023 | Powered by <a href="https://oniontabs.com" target="_blank" rel="noopener noreferrer">Oniontabs LLC</a>
       </div>
     </footer>
   );
