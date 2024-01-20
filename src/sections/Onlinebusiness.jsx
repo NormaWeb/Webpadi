35a0cbe3e06b9ecd14210bdd53959ab52f5c06d3
Blessing Okwong <okwongnorma@gmail.com>
2024-01-20 09:40:16 +0100

    Update Onlinebusiness.jsx

--- src/sections/Onlinebusiness.jsx
+++ src/sections/Onlinebusiness.jsx
@@ -93,26 +93,27 @@ const Onlinebusiness = () => {
 
 
         <div className="flex-1 flex-col ml-10 md:w-2/3 lg:w-1/2">
-        <div className="mt-5 relative">
-          <blockquote className='text-white text-xl relative pl-8'>
-            <span className="absolute left-0 top-0 text-4xl">&ldquo;</span>
-            I can tell you first hand, Basecom is <br /> absolutely amazing. They have helped me <br /> grow my company by 125% since they <br /> revamped our website and messaging. <br /> Highly Recommended!
-          </blockquote>
-        </div>
-
-        <div className='flex '>
-          <div className='flex-1 flex-col'>
-            <img src={Image} alt=""
-              width={100}
-              className="object-contain rounded-2xl mt-8"
-              // className="w-12 h-12 rounded-full object-cover"
-            />
+          <div className="mt-5 relative">
+            <blockquote className='text-white text-xl relative pl-8'>
+              <span className="absolute left-0 top-0 text-4xl">&ldquo;</span>
+              I can tell you first hand, Basecom is <br /> absolutely amazing. They have helped me <br /> grow my company by 125% since they <br /> revamped our website and messaging. <br /> Highly Recommended!
+            </blockquote>
           </div>
-          <div className='flex-1 flex-col mt-8'>
-            <span className=" text-xl text-right   text-white">
-              David Henricks
-            </span>
-            <p className='text-white text-sm mt-1'>CEO, Henricks Enterprise</p>
+
+          <div className='flex '>
+            <div className='flex-1 flex-col'>
+              <img src={Image} alt=""
+                width={100}
+                className="object-contain rounded-2xl mt-8"
+              // className="w-12 h-12 rounded-full object-cover"
+              />
+            </div>
+            <div className='flex-1 flex-col mt-8'>
+              <span className=" text-xl text-right   text-white">
+                David Henricks
+              </span>
+              <p className='text-white text-sm mt-1'>CEO, Henricks Enterprise</p>
+            </div>
           </div>
         </div>
       </div>
